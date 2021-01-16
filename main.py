04413182251f7daf21f45e8ef5f5db99e433065c
VernadskyDanya <d.vernadsky@yandex.ru>
2021-01-16 21:27:53 +0300

    feat: deleting non-actual results

--- main.py
+++ main.py
@@ -1,29 +1,38 @@
-""" Идея 1: из каждого json файла на входе выбираем номер пользователя и открываем по нему результирующую директорию,
-    находим в ней файл с таким же номером и перезаписываем с новыми данными"""
+""" Идея решения: из каждого json файла на входе выбираем номер пользователя и открываем по нему результирующую
+    директорию, находим в ней файл с таким же номером и перезаписываем с новыми данными"""
 
 import json
 import os
 from datetime import datetime
 
 
+def delete_previous(path):
+    """ Функция удаления файлов в директории """
+    file_list = [f for f in os.listdir(path)]
+    for f in file_list:
+        os.remove(os.path.join(path, f))
+
+
 def change_res_file(input_json, res_json):
-    actions_list_input = input_json["sessions"][0]['actions']
-    actions_list_res = res_json['actions']
+    """Функция, которая редактирует результирующий файл на основе входного
+    input_json, res_json - объекты класса json. """
+    actions_list_input = input_json["sessions"][0]['actions']   # Перешли в ветку 'actions'
+    actions_list_res = res_json['actions']      # Перешли в ветку 'actions'
     for action_inp in actions_list_input:
         type = action_inp['type']
-        time = datetime.strptime(action_inp['created_at'][9:-2], '%Y-%m-%dT%H:%M:%S')
+        time_inp = datetime.strptime(action_inp['created_at'][9:-2], '%Y-%m-%dT%H:%M:%S').isoformat() # Читаем и преобразуем время
+        print(time_inp, ' ', type)
         for action_res in actions_list_res:
             if action_res['type'] == type:
-                #print(action_res['count'])
                 action_res['count'] += 1
-
-    """
-    res_file.seek(0)
-    res_file.write(json.dumps(res_json, indent=4)) # Перезаписываем
-    res_file.truncate()
-    res_file.seek(0)
-    print(res_file.read())
-    """
+                time_res = action_res['last']
+                if time_res:    # Если в 'last' уже есть записи
+                    time_res = datetime.strptime(action_res['last'][9:-2], '%Y-%m-%dT%H:%M:%S').isoformat()
+                    if time_inp > time_res:
+                        print(time_inp, ' > ', time_res)
+                        action_res['last'] = "ISODate('" + time_inp + "')"
+                else:   # Если "last": null
+                    action_res['last'] = "ISODate('" + time_inp + "')"
 
 
 def create_pattern_json(number):
@@ -49,7 +58,9 @@ def create_pattern_json(number):
 
 if __name__ == '__main__':
     tree = os.walk('Account')
+    delete_previous("Results-data")     # Удаляем предыдущие результаты, если они есть
     for i in tree:
+        print(i)
         for file_name in i[2]:      # Для каждого файла в каталоге Account
             try:
                 with open(i[0] + '/' + file_name, 'r', encoding='utf8') as input_file:  # Читаем входящий файл
@@ -67,8 +78,5 @@ if __name__ == '__main__':
                         res_file.write(json.dumps(res_json, indent=4))  # Перезаписываем
                         res_file.truncate()
                         res_file.seek(0)
-
-
-
             except IOError:
                 print("An IOError has occurred!")
