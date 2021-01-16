ee2f17c0f13e024fafc03d2996e3695e44b3b7e6
VernadskyDanya <d.vernadsky@yandex.ru>
2021-01-16 21:40:28 +0300

    chore: hidden prints

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def change_res_file(input_json, res_json):
     for action_inp in actions_list_input:
         type = action_inp['type']
         time_inp = datetime.strptime(action_inp['created_at'][9:-2], '%Y-%m-%dT%H:%M:%S').isoformat() # Читаем и преобразуем время
-        print(time_inp, ' ', type)
+        # print(time_inp, ' ', type)
         for action_res in actions_list_res:
             if action_res['type'] == type:
                 action_res['count'] += 1
@@ -29,7 +29,7 @@ def change_res_file(input_json, res_json):
                 if time_res:    # Если в 'last' уже есть записи
                     time_res = datetime.strptime(action_res['last'][9:-2], '%Y-%m-%dT%H:%M:%S').isoformat()
                     if time_inp > time_res:
-                        print(time_inp, ' > ', time_res)
+                        # print(time_inp, ' > ', time_res)
                         action_res['last'] = "ISODate('" + time_inp + "')"
                 else:   # Если "last": null
                     action_res['last'] = "ISODate('" + time_inp + "')"
@@ -60,14 +60,13 @@ if __name__ == '__main__':
     tree = os.walk('Account')
     delete_previous("Results-data")     # Удаляем предыдущие результаты, если они есть
     for i in tree:
-        print(i)
         for file_name in i[2]:      # Для каждого файла в каталоге Account
             try:
                 with open(i[0] + '/' + file_name, 'r', encoding='utf8') as input_file:  # Читаем входящий файл
                     input_json = json.loads(input_file.read())      # Преобразуем его в объект класса json
                     res_path = 'Results-data/' + str(input_json['number']) + '.json'    # Запоминаем путь до результирующего файла
                     if not os.path.isfile(res_path):    # Если рез. файла ещё нет, то создаём и заполняем его по шаблону
-                        print(res_path, ' создали')
+                        # print(res_path, ' создали')
                         with open(res_path, 'w+', encoding='utf8') as res_file:
                             res_file.write(create_pattern_json(input_json['number']))
                     # Начинаем дополнять рез. файл новой информацией
