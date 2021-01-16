94c52b4eef621d51455f7a3dca19a01aa73432a5
VernadskyDanya <d.vernadsky@yandex.ru>
2021-01-16 17:32:16 +0300

    fix: changed realization of checking file for existence

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import os
 
 
 def create_pattern_json(number):
+    """Функция создания шаблона результирущего файла
+        Параметр number - идентификационный номер пользователя"""
     return json.dumps({'number': number,
                                     'actions': [
                                         {'type': 'create',
@@ -23,19 +25,21 @@ def create_pattern_json(number):
                                         ]
                                     }, indent=2)
 
+
 if __name__ == '__main__':
     tree = os.walk('Account')
     for i in tree:
         for file_name in i[2]:      # Для каждого файла в каталоге Account
             try:
                 with open(i[0] + '/' + file_name, 'r', encoding='utf8') as input_file:  # Читаем входящий файл
-                    input_json = json.load(input_file)
-                    res_path = 'Results-data/' + str(input_json['number']) + '.json'    # Путь то результирующего файла
-                    with open(res_path, 'w+', encoding='utf8') as res_file:    # Редактируем результирующий файл
-                        if os.path.getsize(res_path) == 0:
-                            # Если рез. файл ещё пустой, то заполняем его по шаблону
-
+                    input_json = json.load(input_file)      # Преобразуем его в объект класса json
+                    res_path = 'Results-data/' + str(input_json['number']) + '.json'    # Запоминаем путь до результирующего файла
+                    if not os.path.isfile(res_path):    # Если рез. файл ещё пустой, то заполняем его по шаблону
+                        with open(res_path, 'w+', encoding='utf8') as res_file:
                             res_file.write(create_pattern_json(input_json['number']))
+                    else:   # Если рез. файл уже создан, то начинаем его дополнять новой информацией
+                        print('a')
+                        #print(input_json['sessions']['actions'])
 
             except IOError:
                 print("An IOError has occurred!")
